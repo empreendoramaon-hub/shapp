249f97e8f1da4889dda5a49098f5fac67b87a003
empreendoramaon-hub <empreendoramaon@gmail.com>
2026-06-28 15:07:47 -0300

    Add service worker

---
+++ public/sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'shapp-landing-v1';
+const CORE_ASSETS = ['/', '/icon.svg', '/manifest.webmanifest'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key))))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then((res) => res || caches.match('/'))));
+});
